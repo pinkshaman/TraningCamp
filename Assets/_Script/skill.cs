eaed56c0a31edfcb80d39bacf61c365f5f261645
Le Hieu <hieupl12@gmail.com>
2024-07-13 05:21:05 +0200

    123.1

--- Assets/_Script/skill.cs
+++ Assets/_Script/skill.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 
 public class skill : MonoBehaviour
@@ -8,24 +9,34 @@ public class skill : MonoBehaviour
     public string skillDescription;
     public string skillType;
     public string skillLevel;
-    
+
     public Collider2D effect;
-    public Vector3 skillRange;    
-    
-    private Animator anim; 
+    public Vector3 skillRange;
+
+    private Animator anim;
     private bool hit;
     public float speed = 30.0f;
     float direction;
 
-  
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    hit = true;
+    //    anim.SetBool("isOnHit", true);
+    //    anim.SetTrigger("onHit");
+    //    effect.enabled = false;
+
+    //}
+    void onHitDefi()
     {
-        hit = true;       
-        anim.SetBool("isOnHit",true);
-        anim.SetTrigger("explosion");
-        effect.enabled = false;
-
+        hit = true;
+        if (effect.IsTouchingLayers())
+        {
+            anim.SetBool("isOnHit", true);
+            anim.SetTrigger("onHit");
+            effect.enabled = false;
+        }
     }
 
 
@@ -59,12 +70,14 @@ public class skill : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        OnTriggerEnter2D(effect);
+
         if (hit) return;
         float skillSpeed = speed * Time.deltaTime * direction;
         transform.Translate(skillSpeed, 0.0f, 0.0f);
         anim.SetTrigger("FireBall");
-       
+        //OnTriggerEnter2D(effect);
+        onHitDefi();
+
     }
 
 }
