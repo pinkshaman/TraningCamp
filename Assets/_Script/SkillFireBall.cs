77e0c45f37afc9b442a34ceba5c04948931abf66
le hieu <hieupl12@gmail.com>
2024-08-29 21:19:27 +0700

    1

--- Assets/_Script/SkillFireBall.cs
+++ Assets/_Script/SkillFireBall.cs
@@ -7,37 +7,35 @@ using UnityEngine.Windows;
 
 public class SkillFireBall : MonoBehaviour
 {
-    public Player setDirection;
+ 
     public Collider2D effect;
     private Animator anim;
     private bool hit;
     public float speed = 30.0f;
-    public float timeDestroy = 0.25f;
+    public float direction;
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Debug.Log("onHit");
         hit = true;
         effect.enabled = false;
-        anim.SetTrigger("onHit");
-        Destroy(gameObject, timeDestroy);
-        //StartCoroutine(Effect());
+        StartCoroutine(Effect());
 
     }
-    //public IEnumerator Effect()
-    //{
-    //    OnHit();
-    //    yield return new WaitForSeconds(0.25f);
-    //    Deactivate();
-    //}
-    //public void OnHit()
-    //{
-    //    anim.SetTrigger("onHit");
-    //}
+    public IEnumerator Effect()
+    {
+        OnHit();
+        yield return new WaitForSeconds(0.25f);
+        Deactivate();
+    }
+    public void OnHit()
+    {
+        anim.SetTrigger("onHit");
+    }
 
     private void Deactivate()
     {
-        Destroy(gameObject, timeDestroy);
+        Destroy(gameObject);
     }
    
     void Start()
